183e98e36e1aba167fa0362fe66938be3343aa07
seehimanshu <himanshu.thakur2812@gmail.com>
2022-10-04 00:08:00 +0530

    functions

--- functions.js
+++ functions.js
@@ -1,12 +1,6 @@
-let j=20;
-var num=20;
-for{var i=0;i<num;i++}{
-    let j=200;
-    if(i%2==0)
-    {
-        console.log(i);
-    }
-  
-    console.log("inner" +j);
+function add(a,b)
+{
+    return a+b;
 }
-console.log(j);
+let ans=add(2,5);
+console.log(ans);
