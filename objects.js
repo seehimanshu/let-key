d8c5740ce178247f655458d8c666343b93572f85
seehimanshu <himanshu.thakur2812@gmail.com>
2022-10-05 00:54:28 +0530

    objects.js

--- objects.js
+++ objects.js
@@ -13,3 +13,32 @@ let person ={
 console.log(person);
 console.log(person.height);
 console.log(`hey ${person.name}, thankyou for sing in up`);
+
+let str="hello";
+console.log(str.length);//dot notation
+console.log(str["length"]);//square bracket notation
+
+//nesting of objects
+let captainAmerica ={
+    firstname:"steve",
+    secondname:"rogers",
+    friends:["bucky","tony stark","brue banner"],
+    age:102,
+    isAvenger:true,
+    adress:{
+        state:"manhattan",
+        city:"newyork",
+        country:"usa"
+    },
+    sayHi: function(){
+        console.log(`hello my name is ${this.firstname}`);
+    }
+};
+
+console.log(captainAmerica);
+console.log(captainAmerica.friends);
+console.log(captainAmerica.friends[0]);
+captainAmerica.sayHi();//method accessing
+
+// methods ->objects k andar functions 
+console.log(str.toUpperCase());
