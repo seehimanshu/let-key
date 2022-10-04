8f8b61832dfe6cedd60583f6c8aa166a1ae943e7
seehimanshu <himanshu.thakur2812@gmail.com>
2022-10-05 00:35:45 +0530

    objects.js

---
+++ objects.js
@@ -0,0 +1,15 @@
+//Javascript objects are always in key value pair 
+let obj={};//empty object
+console.log(obj);
+
+let person ={
+    name:"abhishek",
+    age:28,
+    phone:9999999999,
+    gender:"male",
+    height:"170cm"
+};
+
+console.log(person);
+console.log(person.height);
+console.log(`hey ${person.name}, thankyou for sing in up`);
