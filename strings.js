50ee3187be1af1889d2021e3c766fcbd00c34dac
seehimanshu <himanshu.thakur2812@gmail.com>
2022-10-04 21:00:23 +0530

    Strings.js

--- strings.js
+++ strings.js
@@ -22,3 +22,39 @@ console.log(slicedStr);
 
 var slicedStr=str.slice(4);
 console.log(slicedStr);
+//substring method
+
+// // jo index de rkha h waha se start hoga or jo 2nd number de rkha h utne total no of character add krega
+
+// let ans=substr(2,6);
+// console.log(ans);
+// console.log(str);
+
+str="YOLO";
+console.log(str);
+console.log(str.toLowerCase());
+
+str ="fomo"
+console.log(str);
+console.log(str.toUpperCase());
+
+//concatenation methhod ->2 ko jodta h
+
+let firstStr="you guys ";
+let secondStr="are smart ";
+
+let concatenationStr=firstStr+secondStr;
+console.log(concatenationStr);
+
+// concat method ->end mai jodta h
+let concatStr=firstStr.concat(secondStr,"and me");
+console.log(concatStr);
+
+//trim method ->remove all the white spaces
+let trimStr=        "      hello   how are you      ";
+console.log(trimStr);
+console.log(trimStr.length);
+
+
+console.log(trimStr.trim());
+console.log(trimStr.trim().length);
