c816df07fbd9ef4eaf0a4a20468fcb71b649709a
seehimanshu <himanshu.thakur2812@gmail.com>
2022-10-04 20:19:28 +0530

    strings

---
+++ strings.js
@@ -0,0 +1,18 @@
+// string is a sequence of characters
+
+var str="hello pepcoders";
+console.log(str);
+
+console.log(str[3]);
+console.log(str.length);
+
+//string methods
+
+//1. extraction Method
+
+//slice method
+//jo index likha hoga waha se shuru hoga or last wale se phle tk
+
+let slicedStr=str.slice(6,15);
+console.log(slicedStr);
+console.log(str);
