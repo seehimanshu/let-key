bedc4e1fb3488514420b6057affaea08595f534c
seehimanshu <himanshu.thakur2812@gmail.com>
2022-10-06 18:48:41 +0530

    how js works

---
+++ howJScodewroks.js
@@ -0,0 +1,11 @@
+var num=5;
+function cube(num){
+    return num*num*num;
+}
+var ans1=cube(num);
+var ans2=cube(10);
+var ans3=cube();
+
+console.log(ans1);
+console.log(ans2);
+console.log(ans3);
--- objects.js
+++ objects.js
@@ -53,14 +53,3 @@ for(let abc in captainAmerica){
 delete captainAmerica.lastname;
 console.log(captainAmerica.lastname);
 
-var num=5;
-function cube(num){
-    return num*num*num;
-}
-var ans1=cube(num);
-var ans2=cube(10);
-var ans3=cube();
-
-console.log(ans1);
-console.log(ans2);
-console.log(ans3);
