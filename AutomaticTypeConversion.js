cb20a41da621fe16ed009998101fd5b751296bb6
seehimanshu <himanshu.thakur2812@gmail.com>
2022-10-06 19:15:48 +0530

    AutomaticTypeConversion.js

--- AutomaticTypeConversion.js
+++ AutomaticTypeConversion.js
@@ -4,3 +4,9 @@ console.log(ans);
 var ans=1+"5";
 console.log(ans);
 
+console.log(null *5);//null is converted into 0then ans is 0
+console.log(undefined *5);//0ka multiplication zero hi hota
+console.log("5"-1); //string number bn jaegi fr 5-1=4 ayega
+console.log(null*"5");//0 ans
+console.log("ten"*3);//NaN
+console.log("10"*3);
