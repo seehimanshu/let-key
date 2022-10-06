0fc2b518c1be0a00cf231004f27641fad82c1430
seehimanshu <himanshu.thakur2812@gmail.com>
2022-10-07 02:20:53 +0530

    let,var,const.js

--- let,var,const.js
+++ let,var,const.js
@@ -1,4 +1,17 @@
 console.log(x);
-console.log(b);
+
 var x=10;
-let b=100;
+let d=100;
+console.log(d);
+var a=300;
+{
+    var a=10;
+    let b=20;
+    const c=30;
+
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+console.log(a);
+// console.log(b);
