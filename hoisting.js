2b4190ae758fbcdb9598840dd984b1d23c984702
seehimanshu <himanshu.thakur2812@gmail.com>
2022-10-06 21:49:20 +0530

    hoisting.js

--- hoisting.js
+++ hoisting.js
@@ -1,6 +1,19 @@
-var x=10;
-function greet(){
-    console.log("hello pepcoder");
-}
+// var x=10;
+// function greet(){
+//     console.log("hello pepcoder");
+// }
+// console.log(x);
+// greet();
+
 console.log(x);
 greet();
+// hello();
+var x=10;
+function greet(){
+    
+    console.log("hello pepcoder");
+}
+var hello=function(){
+    console.log("you guys rock");
+}
+hello();
