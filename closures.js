79a2edcb0c37caf4b9b5a27f3c866e39379c8166
seehimanshu <himanshu.thakur2812@gmail.com>
2022-10-10 22:04:33 +0530

    closures.js

--- closures.js
+++ closures.js
@@ -1,8 +1,30 @@
+// function outer() {
+//     var a=10;
+//     function inner(){
+//         console.log(a);
+//     }
+//     inner();
+// }
+// outer();
 function outer() {
     var a=10;
     function inner(){
         console.log(a);
     }
-    inner();
+    return inner;
 }
-outer();
+var z=outer();
+console.log(z);
+z();
+
+function outter(){
+    var a=7;
+    function inner(){
+        console.log(a);
+    }
+    a=11;
+    return inner;
+}
+var c=outter();
+console.log(c);
+c();
