f6d2de8033965757fa45781b4c72b422c6eefeee
seehimanshu <himanshu.thakur2812@gmail.com>
2022-10-10 20:46:53 +0530

    let,var,const.js

--- let,var,const.js
+++ let,var,const.js
@@ -19,3 +19,16 @@ console.log(a);
 var a=10;
 var a=100;//shadowing
 console.log(a);
+
+let c=10;
+var a=2000;
+{
+    var a=10;
+    let b=20;
+    const c=30;
+    console.log(" inner"+a);
+    console.log(b);
+    console.log(c);
+}
+console.log(c);
+console.log(a);
